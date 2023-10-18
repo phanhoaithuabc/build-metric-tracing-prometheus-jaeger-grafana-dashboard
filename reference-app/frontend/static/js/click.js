51c5fe38789a1b87fd1bf1788a72e72a50a36734
Phan Hoai Thu <phanhoaithuabc@gmail.com>
2023-10-18 11:41:19 +0700

    frontend

--- reference-app/frontend/static/js/click.js
+++ reference-app/frontend/static/js/click.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
     // all custom jQuery will go here
     $("#firstbutton").click(function () {
         $.ajax({
-            url: "http://backend.default.svc.cluster.local", success: function (result) {
+            url: "http://backend-service.default.svc.cluster.local:8081", success: function (result) {
                 $("#firstbutton").toggleClass("btn-primary:focus");
                 }
         });
     });
     $("#secondbutton").click(function () {
         $.ajax({
-            url: "http://trial.default.svc.cluster.local", success: function (result) {
+            url: "http://trial-service.default.svc.cluster.local:8082", success: function (result) {
                 $("#secondbutton").toggleClass("btn-primary:focus");
             }
         });
